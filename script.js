91c1b8cc1975e92da7b269549e15017f4102819e
root <root@DESKTOP-HEVVV59.localdomain>
2022-12-06 00:14:31 +0530

    Calculator Files

---
+++ script.js
@@ -0,0 +1,25 @@
+let string = "";
+let buttons = document.querySelectorAll('.button');
+Array.from(buttons).forEach((button)=>{
+  button.addEventListener('click', (e)=>{
+    if(e.target.innerHTML == '='){
+      string = eval(string);
+      document.querySelector('input').value = string;
+    }else if(e.target.innerHTML == 'C'){
+        string = "";
+        document.querySelector('input').value = string;
+    }else{
+      console.log(e.target);
+      if(e.target.innerHTML == 'X'){
+        string = string + '*';
+        document.querySelector('input').value = string;
+      }else if(e.target.innerHTML == '%'){
+        string = string + '/100';
+        document.querySelector('input').value = string;
+      }else{
+        string = string + e.target.innerHTML;
+        document.querySelector('input').value = string;
+      }
+    }
+  })
+})
